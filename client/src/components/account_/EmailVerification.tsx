7e0d9980740b8884cb7b3de031b52fca2e7fe5c3
nizam-polus <144770483+nizam-polus@users.noreply.github.com>
2024-01-05 14:03:48 +0530

    - correction in verify page

--- client/src/components/account_/EmailVerification.tsx
+++ client/src/components/account_/EmailVerification.tsx
@@ -19,14 +19,14 @@ function EmailVerification() {
             verifyKey: key
         };
         APIs.verifyUser(userData).then((response: any) => {
-            let verified = response.data.verified;
+            let isVerified = response.data.verified; 
             console.log(response.data);
-            if (verified) {
-                setVerified(verified);
+            if (isVerified == true) {
+                setVerified(true);
                 setTimeout(() => {
                     router.push({ pathname: '/homepage', query: { emailVerified: true, username: username } });
                 }, 1500);
-            } else if (response.data === "Already verified") {
+            } else if (response.data === "Alredy verified") {
                 setAlreadyVarified(true);
             }
         });
@@ -36,23 +36,23 @@ function EmailVerification() {
         <>
             <div style={{ textAlign: 'center', position: 'relative', marginTop: '10%' }}>
             {!verified ? (
-                alreadyVarified ? (
+                !alreadyVarified ? (
+                    <>
+                        <p className="body-sub-titles-1 regularfont mt-2">
+                            <FormattedMessage id="VARIFYING_EMAIL" />
+                        </p>
+                    </>
+                ) : (
                     <div>
                         <i className="fa fa-check-circle" style={{ fontSize: '4rem', color: 'yellow' }}></i>
                         <p className="body-sub-titles-1 regularfont mt-2"><FormattedMessage id="ALREADY_VARIFIED"/></p>
                     </div>
-                ) : (
-                    <>
-                        <p className="body-sub-titles-1 regularfont mt-2">
-                            <FormattedMessage id="VERIFY_EMAIL" />
-                        </p>
-                    </>
                 )
             ) : (
                 <div>
                     <i className="fa fa-check-circle" style={{ fontSize: '4rem', color: '#587E50' }}></i>
                     <p className="body-sub-titles-1 regularfont mt-2">
-                        <FormattedMessage id="VERIFY_SUCCESS" />
+                        <FormattedMessage id="VARIFY_SUCCESS" />
                     </p>
                     <p className="body-sub-titles-2 regularfont m-0">
                         <FormattedMessage id="REDIRECT_HOME" />
