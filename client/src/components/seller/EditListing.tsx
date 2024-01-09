736db7474f9912b469080d2c8fb0e5ba85a971f3
nizam-polus <144770483+nizam-polus@users.noreply.github.com>
2024-01-09 11:07:00 +0530

    image zoom function added
    - Zoom image in productsingle page and sellerListpage
    -sticker changes
    - loading ui added in shop page

--- client/src/components/seller/EditListing.tsx
+++ client/src/components/seller/EditListing.tsx
@@ -780,7 +780,7 @@ function EditListing() {
                                                             className='d-flex align-items-center justify-content-center flex-column'
                                                             ref={componentRef}
                                                             >
-                                                            <div className="details d-flex" style={{ width: "100%", fontWeight: "600", fontSize: "16px" }}>
+                                                            <div className="details d-flex" style={{ width: "100%", fontWeight: "500", fontSize: "16.3px" }}>
                                                                 <div className='d-flex justify-content-between' style={{ minWidth: "180px" }}>
                                                                     {listBarcode && <Qrgenerator qrValue={listBarcode} />}
                                                                 </div>
